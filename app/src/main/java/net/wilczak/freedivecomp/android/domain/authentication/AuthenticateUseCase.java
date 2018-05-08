779e439c0f4b5c3ec59f22472b0f6134d2abab6a
Milan Wilczak <milan.wilczak@gmail.com>
2018-05-08 15:21:57 +0200

    Authentication domain service

--- app/src/main/java/net/wilczak/freedivecomp/android/domain/authentication/AuthenticateUseCase.java
+++ app/src/main/java/net/wilczak/freedivecomp/android/domain/authentication/AuthenticateUseCase.java
@@ -2,22 +2,138 @@ package net.wilczak.freedivecomp.android.domain.authentication;
 
 import net.wilczak.freedivecomp.android.domain.race.Race;
 import net.wilczak.freedivecomp.android.domain.race.RaceRepository;
+import net.wilczak.freedivecomp.android.remote.messages.AuthenticateRequestDto;
+import net.wilczak.freedivecomp.android.remote.remoteservice.RemoteService;
+import net.wilczak.freedivecomp.android.remote.remoteservice.RemoteServiceProvider;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.Single;
 
 public class AuthenticateUseCase {
+    private final String deviceId;
     private final RaceRepository raceRepository;
+    private final RemoteServiceProvider remoteServiceProvider;
+    private final long delay;
 
-    public AuthenticateUseCase(RaceRepository raceRepository) {
+    public AuthenticateUseCase(String deviceId, RaceRepository raceRepository, RemoteServiceProvider remoteServiceProvider, long delay) {
+        this.deviceId = deviceId;
         this.raceRepository = raceRepository;
+        this.remoteServiceProvider = remoteServiceProvider;
+        this.delay = delay;
     }
 
-    public Observable<Race> observeRace(Race race) {
-        return raceRepository.getSavedRace(race.getRaceId());
+    public Observable<Status> authenticateRace(Race race) {
+        Status status = Status.initialize(race);
+        if (status.isAuthenticated()) return Observable.just(status);
+        return authenticateRaceStep(status).share();
     }
 
-    public Single<Race> authenticateRace(Race race) {
-        return Single.error(new UnsupportedOperationException());
+    private Observable<Status> authenticateRaceStep(Status previousStatus) {
+        return Observable
+                .defer(() -> {
+                    RemoteService remoteService = remoteServiceProvider.getService(previousStatus.getRace().getUri());
+                    AuthenticateRequestDto request = new AuthenticateRequestDto().setDeviceId(deviceId).setConnectCode(previousStatus.getConnectCode());
+                    return remoteService.postAuthenticate(previousStatus.getRace().getRaceId(), request).toObservable();
+                })
+                .flatMap(response -> raceRepository.getSavedRace(previousStatus.getRaceId()).take(1).flatMap(loaded -> {
+                    Race savedRace = new Race(loaded).setAuthenticationToken(response.getAuthenticationToken()).setConnectCode(response.getConnectCode());
+                    State newState = response.getAuthenticationToken() == null ? State.BETWEEN_ATTEMPTS : State.AUTHENTICATED;
+                    return raceRepository
+                            .saveRace(savedRace)
+                            .andThen(Observable.just(previousStatus.update(savedRace, newState)))
+                            .startWith(previousStatus.busy());
+                }))
+                .onErrorReturn(previousStatus::fail)
+                .startWith(previousStatus.busy())
+                .flatMap(status -> {
+                    if (status.getState() == State.AUTHENTICATED) {
+                        return Observable.just(status);
+                    } else if (status.getState() == State.FAILED) {
+                        return Observable
+                                .timer(delay * status.getFailedAttempts(), TimeUnit.SECONDS)
+                                .flatMap(t -> authenticateRaceStep(status));
+                    } else {
+                        return Observable
+                                .timer(delay, TimeUnit.SECONDS)
+                                .flatMap(t -> authenticateRaceStep(status));
+                    }
+                });
     }
+
+    public static class Status {
+        private final Race race;
+        private final State state;
+        private final Throwable error;
+        private final int failedAttempts;
+
+        public static Status initialize(Race race) {
+            State initialState = race.getAuthenticationToken() == null ? State.STARTING : State.AUTHENTICATED;
+            return new Status(race, initialState, null, 0);
+        }
+
+        private Status(Race race, State state, Throwable error, int failedAttempts) {
+            this.race = race;
+            this.state = state;
+            this.error = error;
+            this.failedAttempts = failedAttempts;
+        }
+
+        public Race getRace() {
+            return race;
+        }
+
+        public State getState() {
+            return state;
+        }
+
+        public Throwable getError() {
+            return error;
+        }
+
+        public int getFailedAttempts() {
+            return failedAttempts;
+        }
+
+        public String getRaceId() {
+            return race.getRaceId();
+        }
+
+        public String getConnectCode() {
+            return race.getConnectCode();
+        }
+
+        public String getAuthenticationToken() {
+            return race.getAuthenticationToken();
+        }
+
+        public boolean isAuthenticated() {
+            return race.getAuthenticationToken() != null;
+        }
+
+        public Status fail(Throwable t) {
+            return new Status(race, State.FAILED, t, failedAttempts + 1);
+        }
+
+        public Status to(State state) {
+            return new Status(race, state, null, 0);
+        }
+
+        public Status update(Race race, State state) {
+            return new Status(race, state, null, 0);
+        }
+
+        public Status busy() {
+            return new Status(race, State.BUSY, null, 0);
+        }
+    }
+
+    public enum State {
+        STARTING,
+        BETWEEN_ATTEMPTS,
+        BUSY,
+        AUTHENTICATED,
+        FAILED,
+    }
+
 }
